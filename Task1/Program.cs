3fbd0239b6802ba84df79adef893dbc7bed978c0
Dmitrii Priimak <priimak69@inbox.ru>
2023-07-05 13:15:47 +0700

    Tasks

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,39 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-Console.WriteLine("Hello, World!");
+double [,] Create2DArrey ()
+{
+    Console.WriteLine("Enter count of rows: ");
+    int userRows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter count of collumns: ");
+    int userColls = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter min possible value: ");
+    double userMin = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter max possible value: ");
+    double userMax = Convert.ToDouble(Console.ReadLine());
+
+    double [,] created2DArrey = new double [userRows, userColls];
+
+    for (int i = 0; i < userRows; i++)
+    {
+        for (int j = 0; j < userColls; j++)
+        {
+            created2DArrey[i,j] = new Random().NextDouble() * 100;
+        }
+    }
+    return created2DArrey;
+}
+
+void Print2DArrey(double [,] arrey2D)
+{
+    for (int i = 0; i <  arrey2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrey2D.GetLength(1); j++)
+        {
+            Console.Write(arrey2D[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double [,] new2DArrey = Create2DArrey();
+Print2DArrey (new2DArrey);
