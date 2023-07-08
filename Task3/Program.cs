d83cdd08cd19a34a996c5cd7ce98a98200bac469
Dmitrii Priimak <priimak69@inbox.ru>
2023-07-08 16:00:20 +0700

    Tasks

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,56 @@
 ﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-Console.WriteLine("Hello, World!");
+int [,] Create2DArrey ()
+{
+    Console.WriteLine("Enter count of rows: ");
+    int userRows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter count of collumns: ");
+    int userColls = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter min possible value: ");
+    int userMin = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter max possible value: ");
+    int userMax = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    int [,] created2DArrey = new int [userRows, userColls];
+
+    for (int i = 0; i < userRows; i++)
+    {
+        for (int j = 0; j < userColls; j++)
+        {
+            created2DArrey[i,j] = new Random().Next(userMin, userMax + 1);
+        }
+    }
+    return created2DArrey;
+}
+
+void PrintArrey(int [,] arrey)
+{
+    for (int i = 0; i <  arrey.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrey.GetLength(1); j++)
+        {
+            Console.Write(arrey[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintArif(int [,] arreyAr)
+{
+    
+    for (int j = 0; j < arreyAr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arreyAr.GetLength(0); i++)
+        {
+            sum = (arreyAr[i, j] + sum);
+        }
+        double arif = sum / arreyAr.GetLength(0);
+        Console.Write(arif + " ");
+    }
+}
+
+int [,] newArrey = Create2DArrey();
+PrintArrey (newArrey);
+PrintArif (newArrey);
